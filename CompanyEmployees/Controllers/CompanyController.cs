fc292967775af4801d1dd4b3f121d94598c96f0e
Syabra <42738944+Syabra@users.noreply.github.com>
2022-05-19 19:58:56 +0200

    Add delete method to company

--- CompanyEmployees/Controllers/CompanyController.cs
+++ CompanyEmployees/Controllers/CompanyController.cs
@@ -113,5 +113,21 @@ namespace CompanyEmployees.Controllers
             return CreatedAtRoute("CompanyCollection", new {ids}, companyCollectionToReturn);
 
         }
+
+        [HttpDelete]
+        public IActionResult DeleteCompany(Guid id)
+        {
+            var company = _repository.Company.GetCompany(id, trackChanges: false);
+            if(company == null)
+            {
+                _logger.LogError($"Company with id: {id} doesn't exist in the database");
+                return NotFound();
+            }
+
+            _repository.Company.DeleteCompany(company);
+            _repository.Save();
+
+            return NoContent();
+        }
     }
 }
