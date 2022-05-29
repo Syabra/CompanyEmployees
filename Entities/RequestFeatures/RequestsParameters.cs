2a9558e91e81b720b0c0e6ec021c447936e1260f
Syabra <42738944+Syabra@users.noreply.github.com>
2022-05-29 12:44:58 +0200

    Implemented filtering in employee controller

--- Entities/RequestFeatures/RequestsParameters.cs
+++ Entities/RequestFeatures/RequestsParameters.cs
@@ -23,6 +23,10 @@ namespace Entities.RequestFeatures
 
     public class EmployeeParameters : RequestsParameters
     {
+        public uint MinAge { get; set; }
+        public uint MaxAge { get; set; } = int.MaxValue;
 
+
+        public bool ValidAgeRange => MinAge < MaxAge;
     }
 }
